613d8ba5ffbfb819944710e8b68359fa4cff2541
hamza <hamza.maidine@gmail.com>
2020-04-12 23:08:34 +0100

    Tp todo

---
+++ TpAngular/Todo-app/src/app/app.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'My Todos';
+
+  todos= [
+    {
+      label: 'Bring Milk',
+      priority: 3,
+      done: false
+    },
+    {
+      label: 'Pay mobile bil',
+      priority: 1,
+      done: true
+    },
+    {
+      label: 'Clean house',
+      priority: 4,
+      done: false
+    },
+    {
+      label: 'Fix the bulb',
+      priority: 5,
+      done: false
+    },
+    {
+      label: 'Call Sam',
+      priority: 1,
+      done: false
+    }
+  ];
+
+  addTodo(newLabel){
+    var newTodo={
+      label: newLabel,
+      priority: 2,
+      done: false
+    }
+  }
+
+  deleteTodo(todo){
+    this.todos = this.todos.filter(t => t.label !== todo.label);
+  }
+}
